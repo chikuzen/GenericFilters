81354f24cd7f54468ee405ec5cca38a2233893d8
Oka Motofumi <chikuzen.mo@gmail.com>
2012-12-16 15:41:00 +0900

    maximum.c: change get_max_u8/get_max_u16 to get_max

--- src/neighbors/maximum.c
+++ src/neighbors/maximum.c
@@ -25,16 +25,7 @@
 #include "neighbors.h"
 
 
-static uint8_t VS_CC get_max_u8(uint8_t x, uint8_t y, uint8_t z)
-{
-    if (x > y) {
-        return x > z ? x : z;
-    }
-    return y > z ? y : z;
-}
-
-
-static uint16_t VS_CC get_max_u16(uint16_t x, uint16_t y, uint16_t z)
+static unsigned VS_CC get_max(unsigned x, unsigned y, unsigned z)
 {
     if (x > y) {
         return x > z ? x : z;
@@ -47,14 +38,14 @@ static void VS_CC
 proc_max_tb_u8(const uint8_t *rt, const uint8_t *rb, int w, uint8_t *dstp)
 {
     uint8_t maxt, maxb;
-    maxt = get_max_u8(rt[0], rt[1], rb[0]);
+    maxt = get_max(rt[0], rt[1], rb[0]);
     dstp[0] = maxt > rb[1] ? maxt : rb[1];
     for (int x = 1; x < w; x++) {
-        maxt = get_max_u8(rt[x - 1], rt[x], rt[x + 1]);
-        maxb = get_max_u8(rb[x - 1], rb[x], rb[x + 1]);
+        maxt = get_max(rt[x - 1], rt[x], rt[x + 1]);
+        maxb = get_max(rb[x - 1], rb[x], rb[x + 1]);
         dstp[x] = maxt > maxb ? maxt : maxb;
     }
-    maxb = get_max_u16(rt[w - 1], rt[w], rb[w - 1]);
+    maxb = get_max(rt[w - 1], rt[w], rb[w - 1]);
     dstp[w] = maxb > rb[w] ? maxb : rb[w];
 }
 
@@ -63,14 +54,14 @@ static void VS_CC
 proc_max_tb_u16(const uint16_t *rt, const uint16_t *rb, int w, uint16_t *dstp)
 {
     uint16_t maxt, maxb;
-    maxt = get_max_u16(rt[0], rt[1], rb[0]);
+    maxt = get_max(rt[0], rt[1], rb[0]);
     dstp[0] = maxt > rb[1] ? maxt : rb[1];
     for (int x = 1; x < w; x++) {
-        maxt = get_max_u16(rt[x - 1], rt[x], rt[x + 1]);
-        maxb = get_max_u16(rb[x - 1], rb[x], rb[x + 1]);
+        maxt = get_max(rt[x - 1], rt[x], rt[x + 1]);
+        maxb = get_max(rb[x - 1], rb[x], rb[x + 1]);
         dstp[x] = maxt > maxb ? maxt : maxb;
     }
-    maxb = get_max_u16(rt[w - 1], rt[w], rb[w - 1]);
+    maxb = get_max(rt[w - 1], rt[w], rb[w - 1]);
     dstp[w] = maxb > rb[w] ? maxb : rb[w];
 }
 
@@ -94,17 +85,17 @@ proc_maximum_8bit(int plane, const VSFrameRef *src, const VSAPI *vsapi,
     dstp += stride;
 
     for (int y = 1; y < h; y++) {
-        uint8_t max0 = get_max_u8(r0[0], r1[0], r2[0]);
-        uint8_t max1 = get_max_u8(r0[1], r1[1], r2[1]);
+        uint8_t max0 = get_max(r0[0], r1[0], r2[0]);
+        uint8_t max1 = get_max(r0[1], r1[1], r2[1]);
         dstp[0] = max0 > max1 ? max0 : max1;
         for (int x = 1; x < w; x++) {
-            max0 = get_max_u8(r0[x - 1], r0[x], r0[x + 1]);
-            max1 = get_max_u8(r1[x - 1], r1[x], r1[x + 1]);
-            uint8_t max2 = get_max_u8(r2[x - 1], r2[x], r2[x + 1]);
-            dstp[x] = get_max_u8(max0, max1, max2);
+            max0 = get_max(r0[x - 1], r0[x], r0[x + 1]);
+            max1 = get_max(r1[x - 1], r1[x], r1[x + 1]);
+            uint8_t max2 = get_max(r2[x - 1], r2[x], r2[x + 1]);
+            dstp[x] = get_max(max0, max1, max2);
         }
-        max0 = get_max_u8(r0[w - 1], r1[w - 1], r2[w - 1]);
-        max1 = get_max_u8(r0[w], r1[w], r2[w]);
+        max0 = get_max(r0[w - 1], r1[w - 1], r2[w - 1]);
+        max1 = get_max(r0[w], r1[w], r2[w]);
         dstp[w] = max0 > max1 ? max0 : max1;
 
         r0 += stride;
@@ -136,17 +127,17 @@ proc_maximum_16bit(int plane, const VSFrameRef *src, const VSAPI *vsapi,
     dstp += stride;
 
     for (int y = 1; y < h; y++) {
-        uint16_t max0 = get_max_u16(r0[0], r1[0], r2[0]);
-        uint16_t max1 = get_max_u16(r0[1], r1[1], r2[1]);
+        uint16_t max0 = get_max(r0[0], r1[0], r2[0]);
+        uint16_t max1 = get_max(r0[1], r1[1], r2[1]);
         dstp[0] = max0 > max1 ? max0 : max1;
         for (int x = 1; x < w; x++) {
-            max0 = get_max_u16(r0[x - 1], r0[x], r0[x + 1]);
-            max1 = get_max_u16(r1[x - 1], r1[x], r1[x + 1]);
-            uint16_t max2 = get_max_u16(r2[x - 1], r2[x], r2[x + 1]);
-            dstp[x] = get_max_u16(max0, max1, max2);
+            max0 = get_max(r0[x - 1], r0[x], r0[x + 1]);
+            max1 = get_max(r1[x - 1], r1[x], r1[x + 1]);
+            uint16_t max2 = get_max(r2[x - 1], r2[x], r2[x + 1]);
+            dstp[x] = get_max(max0, max1, max2);
         }
-        max0 = get_max_u16(r0[w - 1], r1[w - 1], r2[w - 1]);
-        max1 = get_max_u16(r0[w], r1[w], r2[w]);
+        max0 = get_max(r0[w - 1], r1[w - 1], r2[w - 1]);
+        max1 = get_max(r0[w], r1[w], r2[w]);
         dstp[w] = max0 > max1 ? max0 : max1;
 
         r0 += stride;
